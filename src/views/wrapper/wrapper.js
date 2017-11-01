83111f54a317d5c051ee61057ed47c28ac93fd14
Jason Shields <jason@sitbackmedia.com>
2017-11-01 11:57:10 -0700

    Added footer component

--- src/views/wrapper/wrapper.js
+++ src/views/wrapper/wrapper.js
@@ -9,13 +9,15 @@
 */
 
 import HeaderWeb from '@/components/header/header.vue'
+import FooterWeb from '@/components/footer/footer.vue'
 
 export default {
 
   name: 'wrapper',
 
   components: {
-    HeaderWeb
+    HeaderWeb,
+    FooterWeb
   }
 
 }
