33314ec2b2471868b7b8b20853ff2266265cdaf4
Jason Shields <jason@sitbackmedia.com>
2017-11-02 10:23:37 -0700

    added animations to home software section

--- src/views/home/software/software.js
+++ src/views/home/software/software.js
@@ -9,6 +9,19 @@
 
 export default {
 
-  name: 'Software'
+  name: 'Software',
+
+  mounted () {
+    this.scrollReveal.reveal('.sr-section-software-title', {
+      origin: 'bottom',
+      scale: 1
+    })
+
+    this.scrollReveal.reveal('.sr-section-software-subtitle', {
+      origin: 'bottom',
+      delay: 200,
+      scale: 1
+    })
+  }
 
 }
