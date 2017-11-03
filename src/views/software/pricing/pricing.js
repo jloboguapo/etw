ada89c4458347bbf82fdccc5f5ac1932f284b4f7
Jason Shields <jason@sitbackmedia.com>
2017-11-03 15:55:03 -0700

    added pricing tabs

--- src/views/software/pricing/pricing.js
+++ src/views/software/pricing/pricing.js
@@ -10,12 +10,28 @@ export default {
 
   name: 'Pricing',
 
+  data () {
+    return {
+
+      activePricingTab: 'annual'
+
+    }
+  },
+
   mounted () {
     this.scrollReveal.reveal('.sr-pricing-title')
 
     this.scrollReveal.reveal('.sr-pricing-subtitle', {
       delay: 100
     })
+  },
+
+  methods: {
+
+    setPricingTab (activePricingTab) {
+      this.activePricingTab = activePricingTab
+    }
+
   }
 
 }
