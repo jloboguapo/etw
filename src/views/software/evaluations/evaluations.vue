cd784d681ba64010be9010430fe536f9bfbff574
Jason Shields <jason@sitbackmedia.com>
2017-11-03 11:14:03 -0700

    Added shells for software > KPI and Evaluations

---
+++ src/views/software/evaluations/evaluations.vue
@@ -0,0 +1,2 @@
+<template lang="html" src="./evaluations.html"></template>
+<script type="text/javascript" src="./evaluations.js"></script>
--- src/views/software/software.js
+++ src/views/software/software.js
@@ -3,11 +3,12 @@
 | Software page.
 |--------------------------------------------------------------------------
 |
-| The software ETW.
 |
 */
 
+import Evaluations from '@/views/software/evaluations/evaluations.vue'
 import Intro from '@/views/software/intro/intro.vue'
+import Kpi from '@/views/software/kpi/kpi.vue'
 import Notes from '@/views/software/notes/notes.vue'
 import Pricing from '@/views/software/pricing/pricing.vue'
 
@@ -16,7 +17,9 @@ export default {
   name: 'software',
 
   components: {
+    Evaluations,
     Intro,
+    Kpi,
     Notes,
     Pricing
   }
