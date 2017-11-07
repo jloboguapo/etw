49491ceb7b2fd8756453d428ece1401cac73aaef
Jason Shields <jason@sitbackmedia.com>
2017-11-07 15:27:54 -0700

    added comment on contact form

--- src/components/hubspot-contact-form/hubspot-contact-form.js
+++ src/components/hubspot-contact-form/hubspot-contact-form.js
@@ -1,3 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| Hubspot Contact Form.
+|--------------------------------------------------------------------------
+|
+| Had to do some jquery and css overwrites to get this to work.
+| If you create a new form just make sure the javascript is correct.
+| Refer to the hubspot-contact-form.html file for an example
+|
+*/
+
+
+
 export default {
 
 }
