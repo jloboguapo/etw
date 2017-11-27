59900295cb4b8c65df445b649ef05eab6e76f326
Jason Shields <jason.shields@etw.com>
2017-11-27 11:37:02 -0700

    Updated resource page with new links and images

--- src/views/resources/resources.js
+++ src/views/resources/resources.js
@@ -9,62 +9,42 @@ export default {
        */
       resources: [
         {
-          title: 'Implementation Overview',
-          description: 'Information Sheet.',
-          file: '../../../static/resources/Implementation_Overview-1.27.17.pdf',
-          image: '../../../static/resources/images/implementation-overview.jpg',
+          title: 'Complement & Extend your HRIS with ETW',
+          description: 'Discover how HR can act as a partner in driving performance and strategy to fully engage the workforce.',
+          file: 'http://offers.etw.com/complement-your-hris',
+          image: '../../../static/images/resources/hris.jpg',
           tags: [
-            'implementation'
+            'e-books'
           ]
         },
 
         {
-          title: 'Implementation Operational Best Practices',
-          description: 'Information Sheet.',
-          file: '../../../static/resources/Implementation- Operational Best Practices.pdf',
-          image: '../../../static/resources/images/implementation-operational-practices.jpg',
+          title: 'ETW for Enterprises',
+          description: 'Learn to define and track success for each department to drive winning results for your organization.',
+          file: 'http://offers.etw.com/etw-enterprises-ebook',
+          image: '../../../static/images/resources/enterprises.jpg',
           tags: [
-            'implementation'
-          ]
-        },
-
-        {
-          title: 'Why Invest in 1-to-1 Meetings?',
-          description: '1-to-1 Meetings.',
-          file: '../../../static/resources/Why Invest in 1-to-1 Meetings.pdf',
-          image: '../../../static/resources/images/one-to-one.jpg',
-          tags: [
-            'implementation'
-          ]
-        },
-
-        {
-          title: 'Get the Most Out of Salesforce with ETW',
-          description: 'E-book.',
-          file: '../../../static/resources/Ebook-Get the Most out of Salesforce with ETW.pdf',
-          image: '../../../static/resources/images/salesforce.jpg',
-          tags: [
-            'implementation'
-          ]
-        },
-
-        {
-          title: 'Goal and Plan Flexibility with ETW',
-          description: 'Goals and Plans.',
-          file: '../../../static/resources/Goal and Plan Flexibility with ETW.pdf',
-          image: '../../../static/resources/images/plans.jpg',
-          tags: [
-            'implementation'
+            'e-books'
           ]
         },
 
         {
           title: 'Values Versus Observable Behaviors',
-          description: 'E-book.',
-          file: '../../../static/resources/E-Book Values versus Observable Behaviors.pdf',
-          image: '../../../static/resources/images/observable-behavior.jpg',
+          description: 'Discover the value behind clearly defined observable behaviors as they align the entire organization with the desired culture.',
+          file: 'http://offers.etw.com/values-versus-observable-behaviors-e-book',
+          image: '../../../static/images/resources/observable-behavior.jpg',
           tags: [
-            'implementation'
+            'e-books'
+          ]
+        },
+
+        {
+          title: 'Get the Most Out of Salesforce with ETW',
+          description: 'Salesforce is best used as the platform for documenting activity and generating metrics, ETW is better used for taking that information and integrating with the management process to capture useful insights.',
+          file: 'http://offers.etw.com/get-the-most-out-of-salesforce-with-etw-ebook',
+          image: '../../../static/images/resources/salesforce.jpg',
+          tags: [
+            'e-books'
           ]
         },
 
@@ -72,7 +52,7 @@ export default {
           title: 'Good Manager Versus Bad Manager',
           description: 'This whitepaper explores ETW’s ability to support managers at any level. Good, bad or new, our system will help to improve your struggling managers and optimize your successful ones.',
           file: 'http://offers.etw.com/ultimate-guide-how-to-support-your-managers',
-          image: '../../../static/resources/images/good-bad-manager.jpg',
+          image: '../../../static/images/resources/good-bad-manager.jpg',
           tags: [
             'whitepapers'
           ]
@@ -80,9 +60,9 @@ export default {
 
         {
           title: 'ROI Of An Intentional Culture',
-          description: 'This whitepaper examines the impact of intentional culture–a culture that is defined, communicated and integrated into the business management process to drive better results for the organization.',
+          description: 'This whitepaper examines the impact of intentional culture - a culture that is defined, communicated and integrated into the business management process to drive better results for the organization.',
           file: 'http://offers.etw.com/roi-intentional-culture',
-          image: '../../../static/resources/images/roi-intentional-culture.jpg',
+          image: '../../../static/images/resources/roi-intentional-culture.jpg',
           tags: [
             'whitepapers'
           ]
@@ -92,7 +72,7 @@ export default {
           title: 'Develop A Team Of Expert Leaders',
           description: 'This whitepaper explores the importance of developing your leaders beyond a one-size-fits-all training class. Empower the leaders in your organization to produce the best results possible.',
           file: 'http://offers.etw.com/how-to-develop-team-leaders',
-          image: '../../../static/resources/images/team-of-leaders.jpg',
+          image: '../../../static/images/resources/team-of-leaders.jpg',
           tags: [
             'whitepapers'
           ]
@@ -102,7 +82,7 @@ export default {
           title: 'Goal Setting Tips & Tricks',
           description: 'This whitepaper provides the steps and a few tips for setting meaningful goals. Understand what makes a good goal, who should set them, and how often they should be reviewed.',
           file: 'http://offers.etw.com/how-to-create-effective-goals',
-          image: '../../../static/resources/images/goals.jpg',
+          image: '../../../static/images/resources/goals.jpg',
           tags: [
             'whitepapers'
           ]
@@ -112,7 +92,7 @@ export default {
           title: 'Conducting Valuable 1-to-1 Meetings',
           description: 'This whitepaper provides helpful tactics in how to conduct valuable 1-to-1 meetings. Understand how to conduct a 1-to-1 meeting that is constructive, productive, and worth your time.',
           file: 'http://offers.etw.com/rules-to-conducting-meetings',
-          image: '../../../static/resources/images/valuable-one-to-one.jpg',
+          image: '../../../static/images/resources/valuable-one-to-one.jpg',
           tags: [
             'whitepapers'
           ]
@@ -120,11 +100,101 @@ export default {
 
         {
           title: 'OKRs versus KPIs versus MBOs',
-          description: 'This whitepaper will provide comparisons of the different goal-setting methodoliges your organization may use to track performance. Learn what they are, and which would best suit your business needs.',
+          description: 'This whitepaper will provide comparisons of the different goal-setting methodologies your organization may use to track performance. Learn what they are, and which would best suit your business needs.',
           file: 'http://offers.etw.com/executive-guide-okrs-kpis-mbos',
-          image: '../../../static/resources/images/kpi.jpg',
+          image: '../../../static/images/resources/kpi.jpg',
           tags: [
             'whitepapers'
+          ]
+        },
+
+        {
+          title: 'Objectively Scoring Culture',
+          description: 'Learn how to create the driving force for your organizational growth by identifying and measuring the impact of having an intentional culture.',
+          file: 'http://offers.etw.com/objectively-score-culture',
+          image: '../../../static/images/resources/scoring.jpg',
+          tags: [
+            'whitepapers'
+          ]
+        },
+
+        {
+          title: 'A Day in the Life',
+          description: 'Take a look at what kind of information provided by the ETW software could be beneficial in your specific role.',
+          file: 'http://offers.etw.com/a-day-in-the-life-whitepaper',
+          image: '../../../static/images/resources/day-in-life.jpg',
+          tags: [
+            'whitepapers'
+          ]
+        },
+
+        {
+          title: 'Create a Winning Culture',
+          description: 'At the end of the workshop, you will have a clearly defined Mission, Vision and set of Observable Behaviors that can be used to help employees at all levels understand how they can live them to bring value to the organization.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Workshop%20One%20Sheets/Create%20a%20Winning%20Culture%20Workshop.pdf',
+          image: '../../../static/images/resources/winning-culture.jpg',
+          tags: [
+            'interactive workshops'
+          ]
+        },
+
+        {
+          title: 'Connecting Employees to Strategy',
+          description: 'At the end of the workshop Senior Leaders and their Management Teams will understand the processes and practices necessary to create and execute a winning strategy in their organization.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Workshop%20One%20Sheets/Connecting%20Employees%20to%20Strategy%20Workshop.pdf',
+          image: '../../../static/images/resources/connecting-employees.jpg',
+          tags: [
+            'interactive workshops'
+          ]
+        },
+
+        {
+          title: 'Effective Manager Conversation',
+          description: 'At the end of the workshop, you will understand the three most important forms of communication for managers and learn how to most effectively apply these to your daily/weekly/monthly employee interactions.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Workshop%20One%20Sheets/Effective%20Manager%20Conversations%20Workshop.pdf',
+          image: '../../../static/images/resources/manager-conversations.jpg',
+          tags: [
+            'interactive workshops'
+          ]
+        },
+
+        {
+          title: 'Why Invest in 1-to-1 Meetings?',
+          description: 'These days managers are expected to produce better results and get more done in less time, with fewer resources. This trend is not likely to change anytime soon. Effective managers are able to do more with less by focusing their time on only the highest-value activities.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Implementation%20One-sheets/Why%20Invest%20in%201-to-1%20Meetings%20Data%20Sheet.pdf',
+          image: '../../../static/images/resources/one-to-one.jpg',
+          tags: [
+            'implementation'
+          ]
+        },
+
+        {
+          title: 'Goal and Plan Flexibility',
+          description: 'For maximum flexibility and adaptability within your Management Operating System, ETW offers several different goal types, and plan options.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Implementation%20One-sheets/Goal%20and%20Plan%20Flexibility%20Data%20Sheet.pdf',
+          image: '../../../static/images/resources/plan-flexibility.jpg',
+          tags: [
+            'data sheets'
+          ]
+        },
+
+        {
+          title: 'Implementation Operational Best Practices',
+          description: 'ETW provides a platform to track, evaluate and measure employee performance against the major objectives of the organization.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Implementation%20One-sheets/Implementation%20Operational%20Best%20Practices%20Data%20Sheet.pdf',
+          image: '../../../static/images/resources/implementation-operational-practices.jpg',
+          tags: [
+            'implementation'
+          ]
+        },
+
+        {
+          title: 'Adaptive Insights Map',
+          description: 'The Adaptive Insights Map gives users a quick visual representation of where their organization stands in real-time by highlighting the hierarchical relationships between plans and goals, and providing insights into how strategy is being executed on each level.',
+          file: 'https://offers.etw.com/hubfs/Content%20Offers/Implementation%20One-sheets/Adaptive%20Insights%20Map%20Data%20Sheet.pdf',
+          image: '../../../static/images/resources/aim.jpg',
+          tags: [
+            'data sheets'
           ]
         }
 
