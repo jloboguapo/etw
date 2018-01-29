b95208ff22a66f144b6a8dfee586e6b737b1983f
Jason Shields <jason.shields@etw.com>
2018-01-29 11:19:43 -0700

    Added Youtube video to home page intro

--- src/views/home/intro/intro.js
+++ src/views/home/intro/intro.js
@@ -7,13 +7,14 @@
 */
 
 import ModalHubspot from '@/components/modal-hubspot/modal-hubspot.vue'
+import { videoPlayer } from 'vue-video-player'
+import 'videojs-youtube/dist/Youtube.min.js'
 
 export default {
 
-  name: 'Intro',
-
   components: {
-    ModalHubspot
+    ModalHubspot,
+    videoPlayer
   },
 
   data () {
@@ -23,7 +24,26 @@ export default {
        * For showing the contact modal
        * @type {Boolean}
        */
-      isModalActive: false
+      isShowHubspotModal: false,
+
+      /**
+       * Show/hide video modal
+       * @type {Boolean}
+       */
+      isShowVideoModal: false,
+
+      /**
+       * For Alignment video playback
+       * @type {Object}
+       */
+      videoPlayerOptions: {
+        techOrder: ['youtube'],
+        sources: [{
+          type: 'video/youtube',
+          src: 'https://www.youtube.com/watch?v=EakfI3qrPOA'
+        }],
+        fluid: true
+      }
 
     }
   },
@@ -50,9 +70,24 @@ export default {
      * Toggle the contact Modal
      * @return {none}
      */
-    toggleModal () {
-      this.isModalActive = !this.isModalActive
+    toggleHubspotModal () {
+      this.isShowHubspotModal = !this.isShowHubspotModal
+    },
+
+    /**
+     * Show/Hide the intro video modal
+     * @return {none}
+     */
+    toggleVideoModal () {
+      this.isShowVideoModal = !this.isShowVideoModal
+
+      if (!this.isShowVideoModal) {
+        this.$refs.videoPlayer.player.pause()
+      } else {
+        this.$refs.videoPlayer.player.play()
+      }
     }
+
   }
 
 }
