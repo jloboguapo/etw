d5ab4b8854795fe4febb0953f538330ade107766
Blythe McClure <blythe.mcclure@etw.com>
2019-03-26 15:01:47 -0700

    Event Banner + Pop Over Modal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+  	<app-banner />
     <app-header />
     <router-view />
     <app-footer />
@@ -7,12 +8,14 @@
 </template>
 
 <script>
+import AppBanner from '@/components/AppBanner.vue'
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
 
 export default {
 
   components: {
+    AppBanner,
     AppHeader,
     AppFooter
   }
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="p-t-_5 has-text-centered">
+		<a href="https://etw.splashthat.com/" target="blank">
+			<img 
+				src="@/assets/images/events/etw-leaderboard-ad3.jpg"
+                alt="Join us for the Capable Leadership Webinar">
+        </a>
+
+        <div class="modal" v-bind:class="{ 'is-active': isActive }">
+  			<div class="modal-background" v-on:click="closeModal"></div>
+  				<div class="modal-content">
+    			<!-- Any other Bulma elements you want -->
+    			<a href="https://etw.splashthat.com/" target="blank">
+    				<img src="@/assets/images/events/Leadership_pop.png">
+    			</a>
+  				</div>
+ 				 <button class="modal-close is-large" v-on:click="closeModal" aria-label="close"></button>
+			</div>
+
+		</div>
+
+
+</template>
+
+
+
+<script type="text/javascript">
+	export default {
+
+		data () {
+			return{
+				isActive: false
+			}
+		}, 
+
+		created () {
+			setTimeout(function() {
+				this.isActive = true
+			}.bind(this), 3000);
+		}, 
+
+		methods: {
+			closeModal() {
+				this.isActive = false
+			}
+		}
+	}
+
+
+
+</script>
