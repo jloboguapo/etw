a3dd3536f2e490c7b14c10f740d3668233ab8f2a
Blythe McClure <blythe.mcclure@etw.com>
2019-04-08 10:08:50 -0700

    Removed Pop over and event banner

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <app-banner />
     <app-header />
     <router-view />
     <app-footer />
@@ -8,14 +7,12 @@
 </template>
 
 <script>
-import AppBanner from '@/components/AppBanner.vue'
 import AppFooter from '@/components/AppFooter.vue'
 import AppHeader from '@/components/AppHeader.vue'
 
 export default {
 
   components: {
-    AppBanner,
     AppFooter,
     AppHeader
   }
