e1766417c6fa735dc3d23555981a607c2b1f2fbc
jloboguapo <jloboguapo@gmail.com>
2019-06-05 00:09:04 -0700

    change Header to Cheader to avoid any confusion

--- client/js/components/App.js
+++ client/js/components/App.js
@@ -7,7 +7,7 @@ import '../../sass/style.scss';
 export default function App({ children }) {
   return (
     <div className="etw-main-container">
-      {window.location.pathname !== '/' && <Header />}
+      {window.location.pathname !== '/' && <Cheader />}
       {children}
       {window.location.pathname !== '/' && <Footer />}
     </div>
