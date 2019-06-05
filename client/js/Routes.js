76819ead49bbe4683d4caa37dd446defb7cbef8a
jloboguapo <jloboguapo@gmail.com>
2019-06-05 00:17:30 -0700

    point to Home instead of Header

--- client/js/Routes.js
+++ client/js/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
-import Header from './components/Header';
+import Home from './components/Home';
 
 const Routes = () => (
   <BrowserRouter>
@@ -10,7 +10,7 @@ const Routes = () => (
         <Route
           exact
           path="/"
-          render={routeProps => <Header history={routeProps.history} />}
+          render={routeProps => <Home history={routeProps.history} />}
         />
       </Switch>
     </App>
