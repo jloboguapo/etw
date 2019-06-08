68b13e5abccb85cdb2c05e336311a38b399653bf
jloboguapo <jloboguapo@gmail.com>
2019-06-07 19:42:32 -0700

    bring in svg's for logos banner

--- client/js/components/LogosBanner.js
+++ client/js/components/LogosBanner.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class LogosBanner extends Component {
     render(){
         return (
-        <div className='logos-banner'>
-            <p>
-                intentional cultures both big and small leverage us to win
-            </p>
-            <img className='trip-spark-logo' src='/trip-spark-logo.png' />
-            <img className='univar-logo' src='/univar-logo.png' />
-            <img className='verra-mobility-logo' src='/verra-mobility-logo.jpeg' />
-        </div>
+                <div className='logos-banner'>
+                    <p>
+                        intentional cultures both big and small leverage us to win
+                    </p>
+                    <img className='trip-spark-logo' src='./resources/trip-spark-logo.svg' />
+                    <img className='univar-logo' src='./resources/univar-logo.svg' />
+                    <img className='verra-mobility-logo' src='./resources/verra-mobility-logo.svg' />
+                </div>
         );
     };
 };
