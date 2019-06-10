8021bf25757c776145f98952bafde8a5b3ca1ec7
jloboguapo <jloboguapo@gmail.com>
2019-06-10 16:45:47 -0700

    refactor Header

--- client/js/components/Header.js
+++ client/js/components/Header.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render(){
-    return (
-      <div className='etw-header-container'>
-        <img className='etw-logo-dark-on-transparent' src='./resources/etw-logo-dark-on-transparent.svg' />
-        <p>
-          Performance Solutions
-        </p>
-        <p>
-          Leadership Resources
-        </p>
-        <p>
-          more
-        </p>
-        <button
-          className='etw-webinar-button'
-        >
-          JOIN A WEBINAR
-        </button>
-      </div>
-    );
-  };
+const Header = () => {
+  return (
+    <div className="etw-header-container">
+      <img
+        className="etw-logo-dark-on-transparent"
+        src="./resources/etw-logo-dark-on-transparent.svg"
+      />
+      <p>Performance Solutions</p>
+      <p>Leadership Resources</p>
+      <p>more</p>
+      <button type="button" className="etw-webinar-button">
+        JOIN A WEBINAR
+      </button>
+    </div>
+  );
 };
 
 export default Header;
