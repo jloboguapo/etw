979c7ab1bd5f6a7bf6da8fba3cb17d1b185a25dc
jloboguapo <jloboguapo@gmail.com>
2019-06-10 16:46:54 -0700

    refactor logosBanner

--- client/js/components/LogosBanner.js
+++ client/js/components/LogosBanner.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
-class LogosBanner extends Component {
-    render(){
-        return (
-                <div className='logos-banner'>
-                    <p>
-                        intentional cultures both big and small leverage us to win
-                    </p>
-                    <img className='trip-spark-logo' src='./resources/trip-spark-logo.svg' />
-                    <img className='univar-logo' src='./resources/univar-logo.svg' />
-                    <img className='verra-mobility-logo' src='./resources/verra-mobility-logo.svg' />
-                </div>
-        );
-    };
+const LogosBanner = () => {
+  return (
+    <div className="logos-banner-container">
+      <div className="logos-content-wrapper">
+        <div className="logos-text-container">
+          <p>intentional cultures both big and small leverage us to win</p>
+        </div>
+        <div className="logos-svg-container">
+            <div className='trip-spark-container'>
+          <img
+            className="trip-spark-logo"
+            src="./resources/trip-spark-logo.svg"
+          />
+          </div>
+          <img className="univar-logo" src="./resources/univar-logo.svg" />
+          <img
+            className="verra-mobility-logo"
+            src="./resources/verra-mobility-logo.svg"
+          />
+          <div className='logos-empty-div' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogosBanner;
