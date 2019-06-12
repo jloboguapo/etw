f9ee161075eab8df00f0d35e309567737c0544b0
jloboguapo <jloboguapo@gmail.com>
2019-06-11 22:28:56 -0700

    prettier

--- client/js/components/LogosBanner.js
+++ client/js/components/LogosBanner.js
@@ -8,18 +8,18 @@ const LogosBanner = () => {
           <p>intentional cultures both big and small leverage us to win</p>
         </div>
         <div className="logos-svg-container">
-            <div className='trip-spark-container'>
-          <img
-            className="trip-spark-logo"
-            src="./resources/trip-spark-logo.svg"
-          />
+          <div className="trip-spark-container">
+            <img
+              className="trip-spark-logo"
+              src="./resources/trip-spark-logo.svg"
+            />
           </div>
           <img className="univar-logo" src="./resources/univar-logo.svg" />
           <img
             className="verra-mobility-logo"
             src="./resources/verra-mobility-logo.svg"
           />
-          <div className='logos-empty-div' />
+          <div className="logos-empty-div" />
         </div>
       </div>
     </div>
--- client/js/components/Subscribe.js
+++ client/js/components/Subscribe.js
@@ -4,17 +4,14 @@ const Subscribe = props => {
   const [value, setValue] = useState('');
 
   const handleSubmit = e => {
-      e.preventDefault();
-      console.log('this is where it will make the api call to subscribe', value);
-  }
+    e.preventDefault();
+    console.log('this is where it will make the api call to subscribe', value);
+  };
 
   return (
-    <form
-    className="etw-subscribe-container"
-      onSubmit={handleSubmit}
-    >
+    <form className="etw-subscribe-container" onSubmit={handleSubmit}>
       <input
-      className="etw-input"
+        className="etw-input"
         value={value}
         onChange={e => setValue(e.target.value)}
         placeholder="name@company.com"
