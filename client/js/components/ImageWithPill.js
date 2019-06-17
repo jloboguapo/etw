501ffd2664d313cc31cb8a7c9c1ff1a32f0bded8
jloboguapo <jloboguapo@gmail.com>
2019-06-16 22:33:46 -0700

    refactor category pill

--- client/js/components/ImageWithPill.js
+++ client/js/components/ImageWithPill.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CategoryPill from './CategoryPill';
 
-const ImageWithPills = props => {
-  const { imageClass } = props;
-  return (
-    <div className={`insights-images ${imageClass}`}>
-      <CategoryPill width={130} height={60} textPosition={18} />
-    </div>
-  );
-};
+const ImageWithPills = ({ imageClass }) => (
+  <div className={`insights-images ${imageClass}`}>
+    <CategoryPill />
+  </div>
+);
 
 ImageWithPills.propTypes = {
   imageClass: PropTypes.string.isRequired
