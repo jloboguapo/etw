e3a02cc34ccf0f550b761cb3a03c7797108ee225
jloboguapo <jloboguapo@gmail.com>
2019-06-24 23:59:45 -0700

    add resources section to LeadershipResources

--- client/js/components/LeadershipResources.js
+++ client/js/components/LeadershipResources.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Articles from './Articles';
+import ResourcesPage from './ResourcesPage';
 
 const LeadershipResources = () => {
   return (
     <div className="leadership-resources-container">
       <Articles />
+      <ResourcesPage />
     </div>
   );
 };
