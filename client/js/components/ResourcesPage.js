c8a99d16d10ed6d19950f013e7592160bb4af96b
jloboguapo <jloboguapo@gmail.com>
2019-07-02 01:50:41 -0700

    make carousel

--- client/js/components/ResourcesPage.js
+++ client/js/components/ResourcesPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import ImageWithPill from './ImageWithPill';
 import Subscribe from './Subscribe';
 import CallToAction from './CallToAction';
@@ -16,33 +18,95 @@ const ResourcesPage = props => {
           </p>
           <h4>You might also like these resources</h4>
         </div>
-        <div className="etw-resources-card-container">
-          <div className="etw-resources-row">
-            <div className="image-content-wrapper">
-              <ImageWithPill imageClass="hustle" />
-              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-              <CallToAction
-                linkUrl="/"
-                linkName="call to action"
-                arrowClassName="arrow"
-                source="arrow.svg"
-              />
+        <Carousel
+          centerMode={
+            window.matchMedia('(max-width: 768px)').matches ? false : true
+          }
+          centerSlidePercentage={
+            window.matchMedia('(max-width: 768px)').matches ? 80 : 39
+          }
+          selectedItem={1}
+          showThumbs={false}
+        >
+          <div>
+            <div className="etw-resources-row">
+              <div className="image-content-wrapper">
+                <ImageWithPill imageClass="camera-desert" />
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                <CallToAction
+                  linkUrl="/"
+                  linkName="call to action"
+                  arrowClassName="arrow"
+                  source="arrow.svg"
+                />
+              </div>
             </div>
-            <div className="image-content-wrapper">
-              <ImageWithPill imageClass="spacecraft" />
-              <h3>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                pretium mauris nec aliquam porttitor.
-              </h3>
-              <CallToAction
-                linkUrl="/"
-                linkName="call to action"
-                arrowClassName="arrow"
-                source="arrow.svg"
-              />
-            </div>
+            <p className="legend">Legend 1</p>
           </div>
-        </div>
+          <div>
+            <div className="etw-resources-row">
+              <div className="image-content-wrapper">
+                <ImageWithPill imageClass="photo-mountain" />
+                <h3>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus pretium mauris nec aliquam porttitor.
+                </h3>
+                <CallToAction
+                  linkUrl="/"
+                  linkName="call to action"
+                  arrowClassName="arrow"
+                  source="arrow.svg"
+                />
+              </div>
+            </div>
+            <p className="legend">Legend 2</p>
+          </div>
+          <div>
+            <div className="etw-resources-row">
+              <div className="image-content-wrapper">
+                <ImageWithPill imageClass="modern-desk" />
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                <CallToAction
+                  linkUrl="/"
+                  linkName="call to action"
+                  arrowClassName="arrow"
+                  source="arrow.svg"
+                />
+              </div>
+            </div>
+            <p className="legend">Legend 3</p>
+          </div>
+          <div>
+            <div className="etw-resources-row">
+              <div className="image-content-wrapper">
+                <ImageWithPill imageClass="hustle" />
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                <CallToAction
+                  linkUrl="/"
+                  linkName="call to action"
+                  arrowClassName="arrow"
+                  source="arrow.svg"
+                />
+              </div>
+            </div>
+            <p className="legend">Legend 4</p>
+          </div>
+          <div>
+            <div className="etw-resources-row">
+              <div className="image-content-wrapper">
+                <ImageWithPill imageClass="horse-race" />
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                <CallToAction
+                  linkUrl="/"
+                  linkName="call to action"
+                  arrowClassName="arrow"
+                  source="arrow.svg"
+                />
+              </div>
+            </div>
+            <p className="legend">Legend 5</p>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
