c6b4593da5663213e4190394c4b18be06faf4a58
Melanie Cooper <melanie@cooperbold.com>
2019-07-03 15:50:19 -0700

    More refactors for Performance Solutions page elements

--- client/js/components/CombatDisengagement.js
+++ client/js/components/CombatDisengagement.js
@@ -3,182 +3,133 @@ import CallToAction from './CallToAction';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 const CombatDisengagment = () => {
   return (
     <div className="bg-white py-8 py-lg-11">
-        <Container className="combat-disagreement">
+        <Container className="combat-disengagement-container">
             <Row className="justify-content-lg-center text-lg-center">
                 <Col lg={9}>
                     <h2>Actively disengaged employees cost the US <span className="text-info">$438 billion</span> in lost productivity</h2>
                 </Col>
             </Row>
-            <Row className="justify-content-lg-center text-lg-center">
+            <Row className="justify-content-lg-center text-lg-center mb-8 mb-lg-10">
                 <Col lg={6}>
                     <p className="lead">Though it may not be billions, whatever it’s costing your organization, it’s too much.</p>
+                </Col>
+            </Row>
+
+            <Row className="combat-disengagement-charts mb-8 mb-lg-11">
+                <Col lg={4} className="mb-6 mb-lg-0">
+                    <img src="/lack-card-icon.svg" className="mb-7" />
+                    <div className="h3"><strong>86%</strong> of CEOs say they <span className="text-sea">lack the talent</span> to execute their business strategies</div>
+                    <p className="cite-p">
+                        Global Leadership Forecast 2018; DDI, Inc.; The Conference Board Inc., Ernst & Young, LLP
+                    </p>
+                </Col>
+                <Col lg={4} className="mb-6 mb-lg-0">
+                    <img src="/not-engaged-card-icon.svg" className="mb-7" />
+                    <div className="h3"><strong>85%</strong> of worldwide <span className="text-fire">employees are not engaged</span> or actively disengaged at work</div>
+                    <p className="cite-p">
+                        Gallup 2017 State of the American Workplace
+                    </p>
+                </Col>
+                <Col lg={4} className="mb-6 mb-lg-0">
+                    <img src="/no-direction-card-icon.svg" className="mb-7" />
+                    <div className="h3"><strong>78%</strong> of employees believe their leadership team has <span className="text-sun">little to no direction</span></div>
+                    <p className="cite-p">
+                        Gallup 2017 State of the American Workplace
+                    </p>
+                </Col>
+            </Row>
+
+            <Row className="execution-container">
+                <Col lg={6}>
+                    <h2>
+                        We help you develop an execution playbook to combat disengagement
+                        and unlock high performance
+                    </h2>
+                    <p className="lead">
+                        When your people are focused on the right priorities and
+                        practicing the behaviors essential to generating results,
+                        performance follows. These are the building blocks of every
+                        intentional leader and culture.
+                    </p>
+                    <CallToAction
+                        linkUrl="/"
+                        linkName="Download an execution playbook sample"
+                        arrowClassName="arrow"
+                        source="arrow.svg"
+                    />
+                </Col>
+
+                <Col lg={6}>
+                    <Card className="list-card">
+                        <Card.Body>
+                            <h6 className="mb-5">People-first inputs</h6>
+                            <div className="list-card-group">
+                                <img src="/improving-people-icon.svg" />
+
+                                <div>
+                                    <h4>Improving people</h4>
+                                    <p>
+                                        When your people are focused on the right priorities and
+                                        practicing the behaviors essential to generating results,
+                                        performance follows. These are the building blocks of every
+                                        intentional leader and culture.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="list-card-group">
+                                <img src="/aligned-priorities-icon.svg" />
+
+                                <div>
+                                    <h4>Aligned priorities</h4>
+                                    <p>
+                                        As your people are being developed, we also help your
+                                        leadership define a most important number (MIN) which
+                                        everyone can then focus on and advance.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="list-card-group">
+                                <img src="/healthy-practices-icon.svg" />
+
+                                <div>
+                                    <h4>Healthy practices</h4>
+                                    <p>
+                                        From goal-setting to communication, effective planning to
+                                        creative resource allocation, your team's behaviors and
+                                        processes are what lead to organizational change.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <h6 className="mb-5">Performance-focused outputs</h6>
+
+                            <div className="list-card-group">
+                                <img src="/winning-performance-icon.svg" />
+
+                                <div>
+                                    <h4>Winning performance</h4>
+                                    <p>
+                                        Whether it's EBITDA or another number indicative of your
+                                        performance, accelerating your organization's growth is an
+                                        output of intense focus on your key inputs.
+                                    </p>
+                                </div>
+                            </div>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
 
         </Container>
     </div>
   );
-
-
-
-
-
-
-  //   <div className="combat-disengagement-ultimate-container">
-  //     <div className="combat-disengagement-main-container">
-  //       <div className="combat-disengagement-text-container">
-  //         <h2>
-  //           Actively disengaged employees cost the US <span>$438 billion</span>{' '}
-  //           in lost productivity
-  //         </h2>
-  //         <p>
-  //           Though it may not be billions, whatever it's costing your
-  //           organization, it's too much
-  //         </p>
-  //         <br />
-  //       </div>
-  //       <div className="combat-disengagement-card-container">
-  //         <div className="lack-card">
-  //           <img className="lack-card-icon" src="/lack-card-icon.svg" />
-  //           <h4>
-  //             <span className="percent-span">86%</span> of CEOs say they{' '}
-  //             <span className="lack-span">lack the talent</span> to execute
-  //             their business strategy
-  //           </h4>
-  //           <p className="cite-p">
-  //             Global Leadership Forecast 2018, DDI, Inc; The Conference Board
-  //             Inc, Ernst & Young, LLP
-  //           </p>
-  //         </div>
-  //         <div className="not-engaged-card">
-  //           <img
-  //             className="not-engaged-card-icon"
-  //             src="/not-engaged-card-icon.svg"
-  //           />
-  //           <h4>
-  //             <span className="percent-span">85%</span> of worldwide{' '}
-  //             <span>employees are not engaged</span> or activeley disengaged at
-  //             work
-  //           </h4>
-  //           <p className="cite-p">
-  //             Gallup 2017 State of the American Workplace
-  //           </p>
-  //         </div>
-  //         <div className="no-direction-card">
-  //           <img
-  //             className="no-direction-card-icon"
-  //             src="/no-direction-card-icon.svg"
-  //           />
-  //           <h4>
-  //             <span className="percent-span">78%</span> of employees believe
-  //             their leadership team has <span>little to no direction</span>
-  //           </h4>
-  //           <p className="cite-p">
-  //             Gallup 2017 State of the American Workplace
-  //           </p>
-  //         </div>
-  //       </div>
-  //
-  //       <div className="execution-container">
-  //         <div className="playbook-container">
-  //           <h1>
-  //             We help you develop an execution playbook to combat disengagement
-  //             and unlock high-performance
-  //           </h1>
-  //           <p>
-  //             When your people are focused on the right priorities and
-  //             practicing the behaviors essential to generating results,
-  //             performance follows. These are the building blocks of every
-  //             intentional leader and culture.
-  //           </p>
-  //           <CallToAction
-  //             linkUrl="/"
-  //             linkName="download an execution playbook sample"
-  //             arrowClassName="arrow"
-  //             source="arrow.svg"
-  //           />
-  //         </div>
-  //         <div className="inputs-outputs-container">
-  //           <div className="people-performance-containers-wrap">
-  //             <h5>people-first inputs</h5>
-  //             <div className="people-performance-containers">
-  //               <div className="etw-image">
-  //                 <img
-  //                   className="improving-people-icon"
-  //                   src="/improving-people-icon.svg"
-  //                 />
-  //               </div>
-  //               <div className="content-container">
-  //                 <h4>Improving people</h4>
-  //                 <p>
-  //                   When your people are focused on the right priorities and
-  //                   practicing the behaviors essential to generating results,
-  //                   performance follows. These are the building blocks of every
-  //                   intentional leader and culture.
-  //                 </p>
-  //               </div>
-  //             </div>
-  //             <div className="people-performance-containers">
-  //               <div className="etw-image">
-  //                 <img
-  //                   className="aligned-priorities-icon"
-  //                   src="/aligned-priorities-icon.svg"
-  //                 />
-  //               </div>
-  //               <div className="content-container">
-  //                 <h4>Aligned priorities</h4>
-  //                 <p>
-  //                   As your people are being developed, we also help your
-  //                   leadership define a most important number (MIN) which
-  //                   everyone can then focus on and advance.
-  //                 </p>
-  //               </div>
-  //             </div>
-  //             <div className="people-performance-containers">
-  //               <div className="etw-image">
-  //                 <img
-  //                   className="healthy-practices-icon"
-  //                   src="/healthy-practices-icon.svg"
-  //                 />
-  //               </div>
-  //               <div className="content-container">
-  //                 <h4>Healthy practices</h4>
-  //                 <p>
-  //                   From goal-setting to communication, effective planning to
-  //                   creative resource allocation, your team's behaviors and
-  //                   processes are what lead to organizational change.
-  //                 </p>
-  //               </div>
-  //             </div>{' '}
-  //           </div>
-  //           <div className="people-performance-containers-wrap">
-  //             <h5>performance-focused outputs</h5>
-  //             <div className="people-performance-containers">
-  //               <div className="etw-image">
-  //                 <img
-  //                   className="winning-performance-icon"
-  //                   src="/winning-performance-icon.svg"
-  //                 />
-  //               </div>
-  //               <div className="content-container">
-  //                 <h4>Winning performance</h4>
-  //                 <p>
-  //                   Whether it's EBITDA or another number indicative of your
-  //                   performance, accelerating your organization's growth is an
-  //                   output of intense focus on your key inputs.
-  //                 </p>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default CombatDisengagment;
--- client/js/components/ReadyWin.js
+++ client/js/components/ReadyWin.js
@@ -1,33 +1,66 @@
 import React from 'react';
-import Button from './Button';
 import CallToAction from './CallToAction';
 import ReadyWinForm from './ReadyWinForm';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const ReadyWin = () => {
   return (
-    <div className="ready-win-ultimate-container">
-      <div className="ready-win-main-container">
-        <div className="ready-win-form-container">
-          <Button content="partner with etw to strengthen your culture" />
-          <h2>Are you ready to win?</h2>
-          <p>
-            Now is your time to develop high-performance leaders, establish a
-            value-creation mindset, improve enterprise-wide habits, and more.
-            Unintentional leaders need not apply.
-          </p>
-          <ReadyWinForm />
-          <div className="link-container">
-            <CallToAction
-              linkUrl="/"
-              linkName="not convinced? read some case studies"
-              arrowClassName="arrow"
-              source="arrow.svg"
-            />
-          </div>
-        </div>
-      </div>
+    <div className="bg-white py-8 py-lg-10">
+        <Container>
+            <Row className="justify-content-center">
+                <Col lg={8}>
+                    <Card className="text-center">
+                        <Card.Header className="bg-success">
+                            <Card.Title className="text-white">Parter with ETW to strengthen your culture</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <h2>Are you ready to win?</h2>
+                            <p className="lead mb-8">Now is your time to develop high-performance leaders, establish a value-creation mindset, improve enterprise-wide habits, and more. Unintentional leaders need not apply.</p>
+                            <ReadyWinForm />
+                        </Card.Body>
+                        <Card.Footer>
+                            <CallToAction
+                              linkUrl="/"
+                              linkName="not convinced? read some case studies"
+                              arrowClassName="arrow"
+                              source="arrow.svg"
+                            />
+                        </Card.Footer>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     </div>
-  );
+);
+
+
+
+
+  //       <div className="ready-win-form-container">
+  //         <Button content="partner with etw to strengthen your culture" />
+  //         <h2>Are you ready to win?</h2>
+  //         <p>
+  //           Now is your time to develop high-performance leaders, establish a
+  //           value-creation mindset, improve enterprise-wide habits, and more.
+  //           Unintentional leaders need not apply.
+  //         </p>
+  //         <ReadyWinForm />
+  //         <div className="link-container">
+  //           <CallToAction
+  //             linkUrl="/"
+  //             linkName="not convinced? read some case studies"
+  //             arrowClassName="arrow"
+  //             source="arrow.svg"
+  //           />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default ReadyWin;
