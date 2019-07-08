adb5c4cc09a58ccf9a930bb179292dd0146f803e
Melanie Cooper <melanie@cooperbold.com>
2019-07-08 16:17:18 -0700

    Rebuild navbar as Boottrap component

--- client/js/components/Header.js
+++ client/js/components/Header.js
@@ -1,108 +1,91 @@
 import React, { useState } from 'react';
-//import Button from './Button';
-import MenuPerformanceSolutions from './MenuPerformanceSolutions';
-import MenuLeadershipResources from './MenuLeadershipResources';
-import MenuMore from './MenuMore';
-import Banner from './Banner';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Badge from 'react-bootstrap/Badge';
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = useState(false);
-  const [
-    showmenuPerformanceSolutions,
-    setShowmenuPerformanceSolutions
-  ] = useState(false);
-  const [
-    showmenuLeadershipResources,
-    setShowmenuLeadershipResources
-  ] = useState(false);
-  const [showmenuMore, setShowmenuMore] = useState(false);
 
-  return (
-    <header className="header-public">
-        <Banner content="Join ETW at our next intentional leadership network event, September 5, 2019." />
-        <Container>
-            <a className="logo" href="/">
-                <img src="/etw-logo-dark-on-transparent.svg" />
-            </a>
+    return (
+        <Container className="header">
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                     <img src="/etw-logo-dark-on-transparent.svg" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <NavDropdown title="Performance Solutions" id="solutions-nav-dropdown">
+                            <NavDropdown.Item href="/performanceSolutions" className="dropdown-cap">
+                                <img src="/winning-performance-icon.svg" />
+                                <div className="dropdown-cap-text">
+                                    <h4>Performance Solutions</h4>
+                                    <p>Face your challenges head-on</p>
+                                </div>
+                            </NavDropdown.Item>
+                            <Dropdown.Header>By outcome</Dropdown.Header>
+                            <NavDropdown.Item href="#">Strengthen culture</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Accelerate growth</NavDropdown.Item>
+                            <Dropdown.Header>By solution</Dropdown.Header>
+                            <NavDropdown.Item href="#">Leadership development</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Goal definition and reporting</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Behavior and process design</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Performance delivery</NavDropdown.Item>
+                            <Dropdown.Header>Dig deeper</Dropdown.Header>
+                            <NavDropdown.Item href="#">Why ETW</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Clients</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Pricing</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Leadership Resources" id="leadership-nav-dropdown">
+                            <NavDropdown.Item href="/leadershipResources" className="dropdown-cap">
+                                <img src="/winning-performance-icon.svg" />
+                                <div className="dropdown-cap-text">
+                                    <h4>Leadership resources</h4>
+                                    <p>Get equipped for intentional building</p>
+                                </div>
+                            </NavDropdown.Item>
+                            <Dropdown.Header>Popular</Dropdown.Header>
+                            <NavDropdown.Item href="#">
+                                Intentional leadership score
+                                <Badge pill variant="info" className="ml-2">5 Min</Badge>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#">What is the most important number?</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Best habits to strengthen culture</NavDropdown.Item>
+                            <NavDropdown.Item href="#">2019 state of international leadership</NavDropdown.Item>
+                            <NavDropdown.Item href="#">About the ETW approach</NavDropdown.Item>
+                            <Dropdown.Header>Learn more</Dropdown.Header>
+                            <NavDropdown.Item href="#">Articles</NavDropdown.Item>
+                            <NavDropdown.Item href="#">White papers</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Fact sheets</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Checklists</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Case studies</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="More" id="more-nav-dropdown">
+                            <NavDropdown.Item className="dropdown-cap">
+                                <img src="/winning-performance-icon.svg" />
+                                <div className="dropdown-cap-text">
+                                    <h4>More</h4>
+                                    <p>Explore our intentional culture</p>
+                                </div>
+                            </NavDropdown.Item>
+                            <Dropdown.Header>Our company</Dropdown.Header>
+                            <NavDropdown.Item href="#">About ETW</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Our Story</NavDropdown.Item>
+                            <NavDropdown.Item href="#">How we give back</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Careers</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Partners</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Contact us</NavDropdown.Item>
+                        </NavDropdown>
+                        <Button variant="primary" className="navbar-button" href="/">Join a Webinar</Button>
+                    </Nav>
+                </Navbar.Collapse>
 
-            <div className="header-links">
-                <a href="/performanceSolutions">Performance Solutions</a>
-                <a href="/leadershipResources">Leadership Resources</a>
-                <a href="/">More</a>
-                <Button variant="primary" className="button-link" href="/">Join a Webinar</Button>
-            </div>
+            </Navbar>
         </Container>
-    </header>
-
-
-        // <div
-        //   className={`etw-header-links-container ${
-        //     showMobileMenu ? 'burger-expanded' : ''
-        //   } ${
-        //     showmenuPerformanceSolutions
-        //       ? 'menu-performance-solutions-open'
-        //       : ''
-        //   }
-        //   ${showmenuLeadershipResources ? 'menu-leadership-resources-open' : ''}
-        //   ${showmenuMore ? 'menu-more-open' : ''}`}
-        // >
-        //   <a
-        //     className="header-main-links performance-solutions-link"
-        //     onMouseEnter={() => {
-        //       setShowmenuPerformanceSolutions(!showmenuPerformanceSolutions);
-        //     }}
-        //     onMouseLeave={() => {
-        //       setShowmenuPerformanceSolutions(!showmenuPerformanceSolutions);
-        //     }}
-        //     href="/performanceSolutions"
-        //   >
-        //     <p>Performance Solutions</p>
-        //   </a>
-        //   <MenuPerformanceSolutions />
-        //   <a
-        //     className="header-main-links leadership-resources-link"
-        //     onMouseEnter={() => {
-        //       setShowmenuLeadershipResources(!showmenuLeadershipResources);
-        //     }}
-        //     onMouseLeave={() => {
-        //       setShowmenuLeadershipResources(!showmenuLeadershipResources);
-        //     }}
-        //     href="/leadershipResources"
-        //   >
-        //     <p>Leadership Resources</p>
-        //   </a>
-        //   <MenuLeadershipResources />
-        //   <a
-        //     className="header-main-links more-link"
-        //     onMouseEnter={() => {
-        //       setShowmenuMore(!showmenuMore);
-        //     }}
-        //     onMouseLeave={() => {
-        //       setShowmenuMore(!showmenuMore);
-        //     }}
-        //     href="/"
-        //   >
-        //     <p>more</p>
-        //   </a>
-        //   <MenuMore />
-        //   <Button className="button-link" href="/">
-        //     Join a Webinar
-        //   </Button>
-        // </div>
-        // <img
-        //   className={`etw-hammie-burger  ${showMobileMenu ? 'expanded' : ''}`}
-        //   onClick={() => {
-        //     setShowMobileMenu(!showMobileMenu);
-        //   }}
-        //   src="/hamburger.svg"
-        // />
-
-    // </Container>
-    // </div>
-  );
+    );
 };
 
 export default Header;
