7704596c5051328e009af7967ffd98b76b64ff99
Melanie Cooper <melanie@cooperbold.com>
2019-07-16 13:51:55 -0700

    Clear out old components

--- client/js/components/Insights.js
+++ client/js/components/Insights.js
@@ -43,7 +43,7 @@ const Insights = props => {
                 />
               </Card.Body>
             </Card>
-            
+
             <Card className="insight-card">
               <div
                 className="card-img-bg"
@@ -150,98 +150,6 @@ const Insights = props => {
       </Container>
     </div>
   );
-
-  //   <div className="etw-insights-ultimate-container">
-  //     <div className="etw-insights-main-container">
-  //       <div className="etw-insights-text-container">
-  //         <h2>Growth-driving insights</h2>
-  //         <Subscribe />
-  //         <p>
-  //           {' '}
-  //           By subscribing, you agree to our <a href="/">privacy policy.</a>
-  //         </p>
-  //       </div>
-  //       <div className="etw-insights-card-container">
-  //         <div className="etw-insights-row">
-  //           <div className="image-content-wrapper">
-  //             <ImageWithPill imageClass="phone-on-desk" />
-  //             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-  //             <CallToAction
-  //               linkUrl="/"
-  //               linkName="call to action"
-  //               arrowClassName="arrow"
-  //               source="arrow.svg"
-  //             />
-  //           </div>
-  //           <div className="image-content-wrapper">
-  //             <ImageWithPill imageClass="tower" />
-  //             <h3>
-  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-  //               pretium mauris nec aliquam porttitor.
-  //             </h3>
-  //             <CallToAction
-  //               linkUrl="/"
-  //               linkName="call to action"
-  //               arrowClassName="arrow"
-  //               source="arrow.svg"
-  //             />
-  //           </div>
-  //         </div>
-  //         <div className="etw-insights-row">
-  //           <div className="image-content-wrapper full-width">
-  //             <ImageWithPill imageClass="typing-hands" />
-  //             <h2>
-  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-  //               pretium mauris nec aliquam porttitor.
-  //             </h2>
-  //             <CallToAction
-  //               linkUrl="/"
-  //               linkName="call to action"
-  //               arrowClassName="arrow"
-  //               source="arrow.svg"
-  //             />
-  //           </div>
-  //         </div>
-  //         <div className="etw-insights-row">
-  //           <div className="image-content-wrapper">
-  //             <ImageWithPill imageClass="laptop-angle" />
-  //             <h3>
-  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-  //               pretium mauris nec aliquam porttitor.
-  //             </h3>
-  //             <CallToAction
-  //               linkUrl="/"
-  //               linkName="call to action"
-  //               arrowClassName="arrow"
-  //               source="arrow.svg"
-  //             />
-  //           </div>
-  //           <div className="insights-category-container-5">
-  //             <img className="quote" src="/quote.svg" />
-  //             <p className="etw-client-quote">
-  //               ETW has made all the difference in our performance. They are our
-  //               secret sauce.
-  //             </p>
-  //             <h5>high-performance client name</h5>
-  //             <a className="button-link" href="/">
-  //               <Button content="JOIN our next webinar" />
-  //             </a>
-  //             <p>
-  //               Every Tuesday, Thursday, and Friday, designed for C-level
-  //               leaders like you.
-  //             </p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <CallToAction
-  //         linkUrl="/"
-  //         linkName="more insights"
-  //         arrowClassName="arrow"
-  //         source="arrow.svg"
-  //       />
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Insights;
--- client/js/components/ReadyWin.js
+++ client/js/components/ReadyWin.js
@@ -8,59 +8,34 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 const ReadyWin = () => {
-  return (
-    <div className="bg-white py-8 py-lg-10">
-        <Container>
-            <Row className="justify-content-center">
-                <Col lg={8}>
-                    <Card className="text-center">
-                        <Card.Header className="bg-success">
-                            <Card.Title className="text-white">Parter with ETW to strengthen your culture</Card.Title>
-                        </Card.Header>
-                        <Card.Body>
-                            <h2>Are you ready to win?</h2>
-                            <p className="lead mb-8">Now is your time to develop high-performance leaders, establish a value-creation mindset, improve enterprise-wide habits, and more. Unintentional leaders need not apply.</p>
-                            <ReadyWinForm />
-                        </Card.Body>
-                        <Card.Footer>
-                            <CallToAction
-                              linkUrl="/"
-                              linkName="not convinced? read some case studies"
-                              arrowClassName="arrow"
-                              source="arrow.svg"
-                            />
-                        </Card.Footer>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-);
-
-
-
-
-  //       <div className="ready-win-form-container">
-  //         <Button content="partner with etw to strengthen your culture" />
-  //         <h2>Are you ready to win?</h2>
-  //         <p>
-  //           Now is your time to develop high-performance leaders, establish a
-  //           value-creation mindset, improve enterprise-wide habits, and more.
-  //           Unintentional leaders need not apply.
-  //         </p>
-  //         <ReadyWinForm />
-  //         <div className="link-container">
-  //           <CallToAction
-  //             linkUrl="/"
-  //             linkName="not convinced? read some case studies"
-  //             arrowClassName="arrow"
-  //             source="arrow.svg"
-  //           />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+    return (
+        <div className="bg-white py-8 py-lg-10">
+            <Container>
+                <Row className="justify-content-center">
+                    <Col lg={8}>
+                        <Card className="text-center">
+                            <Card.Header className="bg-success">
+                                <Card.Title className="text-white">Parter with ETW to strengthen your culture</Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <h2>Are you ready to win?</h2>
+                                <p className="lead mb-8">Now is your time to develop high-performance leaders, establish a value-creation mindset, improve enterprise-wide habits, and more. Unintentional leaders need not apply.</p>
+                                <ReadyWinForm />
+                            </Card.Body>
+                            <Card.Footer>
+                                <CallToAction
+                                  linkUrl="/"
+                                  linkName="not convinced? read some case studies"
+                                  arrowClassName="arrow"
+                                  source="arrow.svg"
+                                />
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default ReadyWin;
