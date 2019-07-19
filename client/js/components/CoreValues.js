f967b34db0b79bb19d8195fc83fb65875f9f43bf
jloboguapo <jloboguapo@gmail.com>
2019-07-18 23:02:44 -0700

    make CoreValues.js content and full

--- client/js/components/CoreValues.js
+++ client/js/components/CoreValues.js
@@ -1,44 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CoreValuesCard from './CoreValuesCard';
 import ButtonUp from './Button';
+import { useContentful } from '../utils/customHooks';
 
 const CoreValues = () => {
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [button, setButton] = useState('');
+  const [cards, setCards] = useState([]);
+
+  const client = useContentful();
+
+  const fetchData = async () => {
+    const response = await client.getEntries({
+      include: 10,
+      content_type: 'mainLayout'
+    });
+
+    setTitle(response.items[0].fields.title);
+    setSubtitle(response.items[0].fields.subtitle);
+    setButton(response.items[0].fields.button.fields);
+    setCards(response.items[0].fields.cards);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Container className="hero py-8 py-lg-11">
       <Row>
         <Col lg={7} className="pr-lg-10">
-          <h1>Only the intentional become exceptional</h1>
-          <p className="lead">
-            ETW is the Intentional Leadership Platform that high-performance
-            organizations trust to strengthen culture and accelerate growth.
-          </p>
+          <h1>{title}</h1>
+          <p className="lead">{subtitle}</p>
           <ButtonUp
             variant="primary"
             className="button-link"
-            href="/"
-            content="Join our next webinar"
+            href={button.href}
+            content={button.text}
           />
         </Col>
 
         <Col>
-          <CoreValuesCard
-            href="/performancesolutions"
-            src="/etw-strengthen-culture.svg"
-            title="Strengthen culture"
-            subtitle="Develop a value-creation mindset and skill set within all your employees."
-            ctaContent="Put people first"
-          />
-          <CoreValuesCard
-            href="/leadershipresources"
-            src="/etw-accelerate-growth.svg"
-            title="Accelerate growth"
-            subtitle="Clarify and accomplish what's most important across your
-			organization."
-            ctaContent="Keep performance-focused"
-          />
+          {cards.map(card => {
+            return (
+              <CoreValuesCard
+                key={card.sys.id}
+                href={card.fields.href}
+                src={card.fields.icon.fields.file.url}
+                title={card.fields.title}
+                subtitle={card.fields.subtitle}
+                ctaContent={card.fields.cta}
+              />
+            );
+          })}
         </Col>
       </Row>
     </Container>
