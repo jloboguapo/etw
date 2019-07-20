769ed171901f06f41519f7d1bf2810394d5ce9a9
jloboguapo <jloboguapo@gmail.com>
2019-07-19 21:37:20 -0700

    make routes dynamic

--- client/js/Routes.js
+++ client/js/Routes.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
-import HomePage from './components/HomePage';
-import PerformanceSolutions from './components/PerformanceSolutions';
-import LeadershipResources from './components/LeadershipResources';
+import ContentContainer from './components/ContentContainer';
+import { useContentful } from '../js/utils/customHooks';
 
-const Routes = () => (
-  <BrowserRouter>
-    <App>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={routeProps => <HomePage history={routeProps.history} />}
-        />
-        <Route
-          exact
-          path="/performancesolutions"
-          render={routeProps => (
-            <PerformanceSolutions history={routeProps.history} />
-          )}
-        />
-        <Route
-          exact
-          path="/leadershipresources"
-          render={routeProps => (
-            <LeadershipResources history={routeProps.history} />
-          )}
-        />
-      </Switch>
-    </App>
-  </BrowserRouter>
-);
+const Routes = () => {
+  const [pages, setPages] = useState([]);
+
+  const client = useContentful();
+
+  const fetchData = async () => {
+    const response = await client.getEntries({
+      include: 10,
+      sys: {
+        type: 'Link',
+        linkType: 'Entry',
+        id: 'WnYTMvfL7UWue8TvJ3lyD'
+      },
+      content_type: 'pages'
+    });
+    setPages(response.items);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <App>
+        <Switch>
+          {pages.map(page => (
+            <Route
+              exact
+              key={page.fields.path}
+              path={page.fields.path}
+              render={routeProps => (
+                <ContentContainer
+                  history={routeProps.history}
+                  title={page.fields.title}
+                />
+              )}
+            />
+          ))}
+        </Switch>
+      </App>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
