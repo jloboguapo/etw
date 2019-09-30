8766fa7137639fadd3fdbcdd6c6144ded785a72e
jloboguapo <jloboguapo@gmail.com>
2019-09-30 14:47:29 -0700

    remove repository within repository

--- www/client/js/actionCreators/index.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-starter-kit';
-
-export const clearData = createAction('CLEAR_DATA');
-export const setMessageBannerContent = createAction(
-  'SET_MESSAGE_BANNER_CONTENT'
-);
--- www/client/js/utils/customHooks.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'contentful';
-
-export const useContentful = () =>
-  createClient({
-    space: '8r4ccjrv3jhp',
-    accessToken: 'NsCGjckS1lVjwbDFBRVsReHqY00wH-rPK9ECBg6Oqec'
-  });
