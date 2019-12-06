21d8b93857b18ea6987bd66046915d2631f24c8c
jloboguapo <jloboguapo@gmail.com>
2019-12-06 15:22:52 -0700

    make logos banner more mobile friendly

--- client/js/components/Banner.js
+++ client/js/components/Banner.js
@@ -7,6 +7,20 @@ const Banner = props => {
   const [title, setTitle] = useState('');
   const [img, setImg] = useState([]);
   const { id } = props;
+  const [centerMode, setCenterMode] = useState(
+    !window.matchMedia('(min-width: 600px)').matches
+  );
+
+  const resizeEvent = () =>
+    setCenterMode(!window.matchMedia('(min-width: 600px)').matches);
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeEvent);
+
+    return () => {
+      window.removeEventListener('resize', resizeEvent);
+    };
+  }, []);
 
   const client = useContentful();
 
@@ -33,9 +47,11 @@ const Banner = props => {
       <Container>
         <h4>{title}</h4>
         <div className="logos-list">
-          {img.map(img => {
-            return <img key={img.sys.id} src={img.fields.file.url} />;
-          })}
+          {centerMode
+            ? !_isEmpty(img) && <img src={img[1].fields.file.url} />
+            : img.map(img => {
+                return <img key={img.sys.id} src={img.fields.file.url} />;
+              })}
         </div>
       </Container>
     </div>
